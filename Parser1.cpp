dda8b3880099df762981d99516d053ba3d573cdf
Wully1 <MMOGAWU@web.de>
2014-09-24 16:43:31 +0200

    Update Parser1.cpp

--- Parser1.cpp
+++ Parser1.cpp
@@ -1,55 +1,52 @@
 #include<iostream>
-#include"Parser1.cpp"
-#include<cstdio>
-#include<cstring>
 #include<fstream>
+#include<cstring>
 #include<cstdlib>
+
 using namespace std;
-ifstream file;
-
-int firstend=42;
-
-int main()
+char* iPars(char* word)
 {
+ifstream list;
+list.open("Commands.txt",ios::in);
+int m1=0;
+std::string lines[50];
 
-
-std::string File[4];
-file.open("stt.txt",ios::in);
-int num=0;
-
-while(!file.eof())
+while(!list.eof())
 {
-getline(file,File[num]);
-++num;
+getline(list,lines[m1]);
+++m1;
 }
-std::size_t found =File[1].find("\"",firstend);
-char *p1=const_cast <char*> (File[1].c_str());
-for(int i=0;i<firstend;++i)
+
+std::size_t found1[m1];
+char sentence[60][m1];
+char* command[m1];
+for(int i1=0;i1<m1-1;++i1)
 {
-++p1;
+//std::cout<<lines[i1]<<std::endl;
+found1[i1]=lines[i1].find("\"",1);
+strncpy(sentence[i1],lines[i1].c_str(),found1[i1]);
+
+sentence[i1][found1[i1]-1]='\0';
+char *p1=const_cast <char*> (lines[i1].c_str());
+	for (int i2=0;i2<=found1[i1];++i2)
+	{
+	++p1;
+	}
+	command[i1]=p1;
+
+
+std::string sent=sentence[i1];
+std::string wordstr=word;
+//std::cout<<sentence[i1]<<","<<word<<std::endl;
+if(strcmp(sentence[i1],word)==0)
+	{
+	//std::cout<<sent<<std::endl;
+	return command[i1];
+	//system(command[i1]);
+	}
+
 }
-char word[found-firstend-4];
-strncpy(word,p1,found-firstend);
-word[found-firstend]='\0';
-system("clear");
-
-
-
-//system("rm stt.txt");
-FILE* pfile;
-pfile=fopen("Words.txt","w");
-fputs(word,pfile);
-fclose(pfile);
-char * command;
-command=iPars(word);
-
-if (command=="0")
-{
-system("echo 'I dont know what you want from me' | festival --tts");
-}
-else
-{
-//std::cout<<command<<std::endl;
-system(command);
-}
+//std::cout<<"h"<<std::endl;
+command[1]="0";
+return command[1]; //ka wieso NULL geht nicht
 }
